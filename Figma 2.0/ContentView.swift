f9f787022b559f4baf25f3a9cfe69038c97d9efa
Paul Ruvolo <paullundyruvolo@gmail.com>
2023-03-09 11:32:37 -0500

    First Commit -- 3 screens

--- Figma 2.0/ContentView.swift
+++ Figma 2.0/ContentView.swift
@@ -7,15 +7,64 @@
 
 import SwiftUI
 
+enum ScreenSelector {
+    case mainScreen
+    case secondScreen
+    case thirdScreen
+}
+
+class GlobalViewState: ObservableObject {
+    @Published var screen: ScreenSelector = .mainScreen
+    static var shared = GlobalViewState()
+}
+
 struct ContentView: View {
+    @ObservedObject var globalViewState = GlobalViewState.shared
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        switch globalViewState.screen {
+        case .mainScreen:
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                    .padding(20)
+                Text("Hello, world!")
+                Button("Second Screen") {
+                    globalViewState.screen = .secondScreen
+                }
+                Button("Third Screen") {
+                    globalViewState.screen = .thirdScreen
+                }
+            }
+            .padding(100)
+            .background(Color(red: 1, green: 0.7, blue: 0.2))
+        case .secondScreen:
+            SecondScreen()
+        case .thirdScreen:
+            ThirdScreen()
         }
-        .padding()
+        
+    }
+    
+}
+
+struct SecondScreen: View {
+    @ObservedObject var globalViewState = GlobalViewState.shared
+    var body: some View {
+        Text("Testing Second Screen")
+        Button("Back") {
+            globalViewState.screen = .mainScreen
+        }
+    }
+}
+
+struct ThirdScreen: View {
+    @ObservedObject var globalViewState = GlobalViewState.shared
+    var body: some View {
+        Text("Testing Third Screen")
+        Button("Back") {
+            globalViewState.screen = .mainScreen
+        }
     }
 }
 
