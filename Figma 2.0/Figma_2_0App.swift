851ea51fe3231ca54479718a4faa6b878e9902e3
Paul Ruvolo <paullundyruvolo@gmail.com>
2023-03-09 10:39:37 -0500

    Initial Commit

---
+++ Figma 2.0/Figma_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Figma_2_0App.swift
+//  Figma 2.0
+//
+//  Created by Occam Lab on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Figma_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
